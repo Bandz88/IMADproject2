58748c43c4fdd686d95ba43da9f5c4c840eb34b1
Bandz88 <ST10449673@vcconnect.edu.za>
2024-05-03 11:16:23 +0200

    Submission of assignment

---
+++ app/src/main/java/com/example/imadproject2/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.historyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.imadproject2.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val historicalFigures = mapOf(
+        95 to "Nelson Mandela",
+        25 to "to silo",
+        32 to "Alexander the Great",
+        19 to "Joan of Arc",
+        39 to "Cleopatra",
+        67 to "Leonardo Da Vinci",
+        78 to "Mhat",
+        51 to "Napoleon",
+        55 to "Julias Ceaser",
+        65 to "Gangus Khan"
+
+
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ageEditText = findViewById<EditText>(R.id.ageEditText)
+        val generateButton = findViewById<Button>(R.id.generateButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+        val clearButton = findViewById<Button>(R.id.clearButton)
+
+        clearButton.setOnClickListener {
+            ageEditText.text.clear()
+
+
+            generateButton.setOnClickListener {
+                val age = ageEditText.text.toString().toIntOrNull()
+                age?.let {
+                    val figure = historicalFigures[age]
+                    if (figure != null) {
+                        resultTextView.text = "You are $age years old, which is the same age as $figure."
+                    } else {
+                        resultTextView.text =
+                            "There is no historical figure in our records who was $age years old."
+                    }
+                } ?: run {
+                    resultTextView.text = "Please enter a valid age."
+                }
+            }
+        }
+    }
+}
+
